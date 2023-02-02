81a97c4016589b8ae90d57d935db36a11eaebe13
John McDove <96707236+illusies@users.noreply.github.com>
2023-02-01 23:06:01 -0500

    0x16. API advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit. If an invalid
+subreddit is given, the function should return 0.
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    A function that returns the number of subscribers for a given subreddit
+    """
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+                     v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
